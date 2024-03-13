9f9ccc95695ff3d0288cacc4fe90545a72153ad6
mennah4 <mennahjafar@hotmail.com>
2024-03-13 16:49:15 +0300

    Add customer urls

--- backend/settings/urls.py
+++ backend/settings/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('merchant/', include('merchant.urls')),
+    path('customer/', include('customer.urls')),
 ]
