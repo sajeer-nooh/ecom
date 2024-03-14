20538be9e703b9ce39ce316792af68b7229a7034
mennah4 <mennahjafar@hotmail.com>
2024-03-14 03:09:41 +0300

    Create customer registration endpoint

--- backend/customer/urls.py
+++ backend/customer/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from customer.views import GeCustomerAPIView
+from customer.views import GeCustomerAPIView, RegisterCustomerAPIView
 
 urlpatterns = [
-    path('get-customer', GeCustomerAPIView.as_view())
+    path('get-customer', GeCustomerAPIView.as_view()),
+    path('create-customer', RegisterCustomerAPIView.as_view())
 ]
