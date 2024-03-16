f470ea8f9ce5bb285fdc95159e5277307978ea7a
mennah4 <mennahjafar@hotmail.com>
2024-03-16 10:57:05 +0300

    Add customer routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,11 @@ import Categories from './modules/merchant/pages/Categories';
 import { Provider } from 'react-redux';
 import { store } from './redux';
 import SideNav from './components/SideNav';
+import NavBar from './components/NavBar';
+import StoreFront from './modules/customer/pages/StoreFront';
+import CustomerOrders from './modules/customer/pages/Orders';
+import CheckoutPage from './modules/customer/pages/Checkout';
+import ProductDetails from './modules/customer/pages/ProductDetails';
 
 const App = () => {
   const navigate = useNavigate();
@@ -16,41 +21,83 @@ const App = () => {
   return (
     <Provider store={store}>
       <NextUIProvider navigate={navigate}>
-          <Toaster />
-          <div className="max-w-screen h-screen overflow-auto flex">
-            {['/store/home', '/store/products', '/store/orders', '/store/categories'].includes(window.location.pathname) ? <SideNav /> : null}
-            <div className="w-full h-full p-4 sm:ml-64">
-              <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
-                <Routes>
-                  <Route
-                    path="/store/home"
-                    element={
-                      <Home />
-                    }
-                  />
-                  <Route
-                    path="/store/products"
-                    element={
-                      <Products />
-                    }
-                  />
-                  <Route
-                    path="/store/orders"
-                    element={
-                      <Orders />
-                    }
-                  />
-                  <Route
-                    path="/store/categories"
-                    element={
-                      <Categories />
-                    }
-                  />
-                  {['/store/login', '/store'].map(path => <Route key={path} path={path} element={<LoginPage />} />)}
-                </Routes>
+        <Toaster />
+        <div >
+          {['/store/home', '/store/products', '/store/orders', '/store/categories'].includes(window.location.pathname) ?
+            <>
+              <SideNav />
+              <div className="max-w-screen h-screen overflow-auto flex">
+                <div className="w-full h-full p-4 sm:ml-64">
+                  <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+                    <Routes>
+                      <Route
+                        path="/store/home"
+                        element={
+                          <Home />
+                        }
+                      />
+                      <Route
+                        path="/store/products"
+                        element={
+                          <Products />
+                        }
+                      />
+                      <Route
+                        path="/store/orders"
+                        element={
+                          <Orders />
+                        }
+                      />
+                      <Route
+                        path="/store/categories"
+                        element={
+                          <Categories />
+                        }
+                      />
+                    </Routes>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
+            </>
+            : <>
+              <NavBar />
+              <div className="max-w-screen-lg mx-auto h-screen overflow-auto flex">
+                <div className="w-full h-full p-4">
+                  <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+                    <Routes>
+                      <Route
+                        path="/customer/orders"
+                        element={
+                          <CustomerOrders />
+                        }
+                      />
+                      <Route
+                        path="/customer/products"
+                        element={
+                          <StoreFront />
+                        }
+                      />
+                      <Route
+                        path="/customer/checkout"
+                        element={
+                          <CheckoutPage />
+                        }
+                      />
+                      <Route
+                        path="/product/details"
+                        element={
+                          <ProductDetails />
+                        }
+                      />
+                    </Routes>
+                  </div>
+                </div>
+              </div>
+            </>}
+          {/* {['/customer/home', '/customer/products', '/customer/orders', '/customer/categories'].includes(window.location.pathname) ?  : null} */}
+
+
+        </div>
       </NextUIProvider>
     </Provider>
   );
